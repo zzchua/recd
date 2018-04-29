21fdcceb0e3388bacd0af057182f69fb489d8f94
zack-chua <czongzhen@gmail.com>
2018-04-28 19:23:54 -0700

    main app flow icons done

--- src/navigation/MainTabNavigator.js
+++ src/navigation/MainTabNavigator.js
@@ -1,17 +1,63 @@
+import React from 'react';
 import { TabNavigator } from 'react-navigation';
+import { MaterialIcons } from '@expo/vector-icons';
 import FeedScreen from '../screens/FeedScreen';
 import ProfileScreen from '../screens/ProfileScreen';
+import NotificationsScreen from '../screens/NotificationsScreen';
+import SearchScreen from '../screens/SearchScreen';
+
+const iconSize = 25;
 
 export default TabNavigator(
   {
     Feed: {
       screen: FeedScreen,
     },
+    Search: {
+      screen: SearchScreen,
+    },
+    Notifications: {
+      screen: NotificationsScreen,
+    },
     Profile: {
       screen: ProfileScreen,
     },
   },
   {
+    navigationOptions: ({ navigation }) => ({
+      tabBarIcon: ({ tintColor }) => {
+        const { routeName } = navigation.state;
+        let icon;
+        switch (routeName) {
+          case 'Feed':
+            icon = <MaterialIcons name="home" size={iconSize} color={tintColor} />;
+            break;
+          case 'Search':
+            icon = <MaterialIcons name="search" size={iconSize} color={tintColor} />;
+            break;
+          case 'Notifications':
+            icon = <MaterialIcons name="notifications" size={iconSize} color={tintColor} />;
+            break;
+          case 'Profile':
+            icon = <MaterialIcons name="person" size={iconSize} color={tintColor} />;
+            break;
+          default:
+            break;
+        }
+        return icon;
+      },
+    }),
     tabBarPosition: 'bottom',
+    tabBarOptions: {
+      initialRouteName: 'Feed',
+      order: ['Feed, Search', 'Notifications', 'Profile'],
+      showLabel: false,
+      showIcon: true,
+      activeTintColor: 'black',
+      inactiveTintColor: 'gray',
+      style: {
+        backgroundColor: 'white',
+      },
+    },
   },
 );
