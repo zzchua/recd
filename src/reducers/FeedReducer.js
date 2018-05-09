3989c7eb4fe213ac0c98e909b8acdca2f49d2db4
zack-chua <czongzhen@gmail.com>
2018-05-08 17:53:56 -0700

    functionality to send spotify song recommendations

--- src/reducers/FeedReducer.js
+++ src/reducers/FeedReducer.js
@@ -1,17 +1,12 @@
-import { SHOW_RECD_MODAL, HIDE_RECD_MODAL, GET_SPOTIFY_ACCESS_TOKEN_SUCCESS, GET_SPOTIFY_ACCESS_TOKEN_FAILURE } from '../actions/types';
+import { GET_SPOTIFY_ACCESS_TOKEN_SUCCESS, GET_SPOTIFY_ACCESS_TOKEN_FAILURE } from '../actions/types';
 
 const INITIAL_STATE = {
-  recdModalVisible: false,
   spotifyAccessToken: '',
   spotifyTokenError: false,
 };
 
 export default (state = INITIAL_STATE, action) => {
   switch (action.type) {
-    case SHOW_RECD_MODAL:
-      return { ...state, recdModalVisible: true };
-    case HIDE_RECD_MODAL:
-      return { ...state, recdModalVisible: false };
     case GET_SPOTIFY_ACCESS_TOKEN_SUCCESS:
       return { ...state, spotifyAccessToken: action.payload };
     case GET_SPOTIFY_ACCESS_TOKEN_FAILURE:
